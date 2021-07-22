afc9bb1a45d13067307f69c27519191950cb6a53
VeCAD <30369165+VeCAD@users.noreply.github.com>
2021-07-22 09:27:42 +0800

    Update capture.py
    removed max-buffers from pipeline

--- nano/capture.py
+++ nano/capture.py
@@ -21,7 +21,7 @@ def gstreamer_pipeline(
     "nvvidconv flip-method=%d ! "
     "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
     "videoconvert ! "
-    "video/x-raw, format=(string)BGR ! appsink max-buffers=1 drop=True "
+    "video/x-raw, format=(string)BGR ! appsink drop=True "
     % (
         capture_width,
         capture_height,
